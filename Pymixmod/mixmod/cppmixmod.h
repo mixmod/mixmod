e2bed56bcbe49e7ee2a745c383f5ee8f0f43e2ae
Quentin Grimonprez <qgrimonprez@diagrams-technologies.com>
2023-11-24 18:16:10 +0100

    style(Pymixmod): format c++ files

--- Pymixmod/mixmod/cppmixmod.h
+++ Pymixmod/mixmod/cppmixmod.h
@@ -1,7 +1,6 @@
 
-double *new_double_array(int ii){return new double[ii];}
-double **new_double_p_array(int ii){return new double*[ii];}
-double ***new_double_pp_array(int ii){return new double**[ii];}
+double *new_double_array(int ii) { return new double[ii]; }
+double **new_double_p_array(int ii) { return new double *[ii]; }
+double ***new_double_pp_array(int ii) { return new double **[ii]; }
 
-int64_t *new_int64_t_array(int ii){return new int64_t[ii];}
-
+int64_t *new_int64_t_array(int ii) { return new int64_t[ii]; }
